faeb623642f9bc0a967dfbc5c8447db7ca000553
Jared Rodriguez <jared.rodriguez@rackspace.com>
2016-05-26 01:14:36 -0500

    onto tlv and such
    [root@dhcp-20 ~]# ./lldpr
    Got packet, just not the right one: 0x0027
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0027
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0806
    Got packet, just not the right one: 0x0806
    LLDP packet received! Ethernet type code: 0x88cc
    
    Ethernet frame header:
    Destination MAC (this node): 01:80:c2:00:00:0e
    Source MAC: ec:30:91:e6:93:83
    tlv_type: 16 , tlv_length: 110
    Goto bed

--- lldpr.c
+++ lldpr.c
@@ -20,25 +20,46 @@
 
 #define ETH_P_ALL 0x0003
 #define ETH_P_LLDP 0x88CC
+#define MAC_STRING_LEN 18     // FF:FF:FF:FF:FF:FF\0
+
+typedef struct {
+    uint8_t dest[6];
+    uint8_t src[6];
+    uint16_t type;
+} ethernet_header;
+
+typedef struct {
+    uint16_t type_len; // type = 7 bits, len = 9 bits
+    uint8_t * data;
+} lldp_tlv;
 
 
-uint16_t get_ether_type(uint8_t *);
+/* Prototypes */
 
+void mac_address_fmt(uint8_t *addr, char *buff);
+
+/* MAIN */
 
 int main()
 {
-    int sock, status, i;
-    uint8_t *ether_frame;
-    uint16_t ether_type;
+    int sock, status;
 
-    ether_frame = (uint8_t *) malloc(IP_MAXPACKET * sizeof(uint8_t));
+    ethernet_header * eh;
+    lldp_tlv * tlv_data;
+    uint16_t tlv_type;
+    uint16_t tlv_length;
+    uint8_t *packet;
+
+    char dest_address[MAC_STRING_LEN], src_address[MAC_STRING_LEN];
+
+    packet = (uint8_t *) malloc(IP_MAXPACKET * sizeof(uint8_t));
     
-    if(ether_frame == NULL) {
+    if(packet == NULL) {
         perror("Could not allocate memory for packet buffer");
         exit(EXIT_FAILURE);
     }
     
-    memset(ether_frame, 0, IP_MAXPACKET * sizeof(uint8_t));
+    memset(packet, 0, IP_MAXPACKET * sizeof(uint8_t));
 
     sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 
@@ -46,36 +67,49 @@ int main()
         perror("socket() failed");
         exit (EXIT_FAILURE);
     }
-    setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, "eno1", strlen("eno1") + 1);
+    // setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, "eno1", strlen("eno1") + 1);
 
-    status = recv(sock, ether_frame, IP_MAXPACKET, 0);
+
+    eh = (ethernet_header *) packet;
+
+    while(1) {
+        status = recv(sock, packet, IP_MAXPACKET, 0);
+
+        if (status < 0) {
+            perror("Problem getting packet");
+            exit(EXIT_FAILURE);
+        }
+        if (htons(eh->type) == ETH_P_LLDP)
+            break;
+
+        if (htons(eh->type) != 0x0800)
+            printf("Got packet, just not the right one: 0x%04x\n", htons(eh->type));
+
+        memset(packet, 0, IP_MAXPACKET * sizeof(uint8_t));
+    }
 
     close(sock);
 
-    if (status < 0) {
-        perror("Problem getting packet");
-        exit(EXIT_FAILURE);
-    }
-
-    printf ("Ethernet type code: 0x%04x\n", get_ether_type(ether_frame));
+    printf ("LLDP packet received! Ethernet type code: 0x%04x\n", htons(eh->type));
 	printf ("\nEthernet frame header:\n");
-	printf ("Destination MAC (this node): ");
-	for (i=0; i<5; i++) {
-	    printf ("%02x:", ether_frame[i]);
-	}
-	printf ("%02x\n", ether_frame[5]);
-	printf ("Source MAC: ");
-	for (i=0; i<5; i++) {
-	    printf ("%02x:", ether_frame[i+6]);
-	}
-    printf ("%02x\n", ether_frame[11]);
+
+    mac_address_fmt(eh->dest, dest_address);
+    mac_address_fmt(eh->src, src_address);
+
+	printf ("Destination MAC (this node): %s\n", dest_address);
+	printf ("Source MAC: %s\n", src_address);
+
+    tlv_data = (lldp_tlv * ) eh + sizeof(eh);
+
+    tlv_type = tlv_data->type_len >> 9;
+    tlv_length = tlv_data->type_len & 0x01ff;
+
+    printf("tlv_type: %d , tlv_length: %d\n", tlv_type, tlv_length);
+
+    printf("Goto bed \n");
 }
 
-
-uint16_t get_ether_type(uint8_t * ether_frame) {
-    uint16_t * ether_type;
-
-    ether_type = (uint16_t *) &ether_frame[12];
-
-    return htons(*ether_type);
+void mac_address_fmt(uint8_t *addr, char *buff) {
+    snprintf(buff, MAC_STRING_LEN, "%02x:%02x:%02x:%02x:%02x:%02x",
+            addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
 }
